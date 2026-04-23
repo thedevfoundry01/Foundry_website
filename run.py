aac385818867aff4b568fd4cae08a0fd5bffbfd0
thedevfoundry01 <thedevfoundry01@gmail.com>
2026-04-23 12:03:36 +0530

    fix: remove BOM from requirements.txt

--- run.py
+++ run.py
@@ -2,11 +2,10 @@ import os
 from dotenv import load_dotenv
 from app import create_app
 
-# Load environment variables from .env
 load_dotenv()
 
 app = create_app()
 
 if __name__ == '__main__':
-    debug_mode = os.getenv('FLASK_DEBUG', 'true').lower() == 'true'
-    app.run(debug=debug_mode)
+    debug_mode = os.getenv('FLASK_DEBUG', 'false').lower() == 'true'
+    app.run(host="0.0.0.0", port=5000, debug=debug_mode)
